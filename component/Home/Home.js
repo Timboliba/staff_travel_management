fec6beca54d6bb76d050b1e56e836ad2de91fa34
Hamadi Timboliba <hamaditimboliba54@gmail.com>
2023-03-28 17:08:06 +0000

    Finalisation de la composante parametres avec les listes déroulantes.

--- component/Home/Home.js
+++ component/Home/Home.js
@@ -7,72 +7,13 @@ import SoumettreUneDemande from '../SoumettreUneDemande';
 
 const Home = () => {
 
-    //Création d'un lien home
-    const ouvrirURLHome = () => {
-        Linking.openURL('/');
-        alert("Vous avez cliquer sur Home");
-    };
-
-    //Création d'un lien mes demandes
-    const ouvrirURLMesDeamandes = () => {
-        Linking.openURL('/');
-        alert("Ouvrir mes demande");
-    };
-
-    //Création d'un lien soumettre une demande
-    //<Icon name="home" />
-    const ouvrirURLSoumettreDemande = () => {
-        Linking.openURL('/');
-        alert("Soumettre une demande");
-    };
-
-    //Création d'un lien pour consulter son profil
-    const ouvrirURLProfil = () => {
-        Linking.openURL('./MesDemandes');
-        alert("Vous avez cliquer sur Profil");
-    };
 
     return <>
-        <View style={styleHome.navBar}>
-            <Link to="/">
-                <Text style={styleHome.texte} onPress={ouvrirURLHome}>Home</Text>
-            </Link>
-            <Link to="/Profil">
-                <Text style={styleHome.texte} onPress={ouvrirURLProfil}>Profil</Text>
-            </Link>
-            <Link to="/SoumettreUneDemande">
-                <Text style={styleHome.texte} onLongPress={ouvrirURLSoumettreDemande}>Soumettre une demande</Text>
-            </Link>
-            <Link to="/MesDemandes">
-                <Text style={styleHome.texte} onPress={ouvrirURLMesDeamandes}>Mes Demandes</Text>
-            </Link>
-        </View>
-        <View name="section" style={styleHome.section}>
-
-        </View>
     </>;
 }
 
 const styleHome = StyleSheet.create({
-    navBar: {
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        backgroundColor: "white",
-        width: '100%',
-        height: '10%',
-        paddingTop: 50
-    },
-    section: {
-        width: "100%",
-        backgroundColor: 'green',
-        height: "90%"
-    },
-    texte: {
-        fontWeight: 'bold',
-        fontSize: '13'
-    },
+
 });
 
 export default Home;
--- component/Parametres/Parametres.js
+++ component/Parametres/Parametres.js
@@ -1,17 +1,54 @@
 import { Text, TextInput, View, Image, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { RadioButton } from 'react-native-paper';
 
 const Parametres = () => {
+    const [menuVisible, setMenuVisible] = useState(false);
+    const [langueVisible, setLangueVisible] = useState(false);
+    const toggleMenu = () => {
+        setMenuVisible(!menuVisible);
+    };
+
+    const toggleLangue = () => {
+        setLangueVisible(!langueVisible);
+    };
     return <>
         <View style={{ height: "100%" }}>
-            <View style={StyleParametre.parametreStyle}>
-                <Ionicons name="shield-checkmark-outline" size={27} color='red' /><Text style={StyleParametre.textPara}>Sécurité</Text>
+            <View>
+                <View style={StyleParametre.parametreStyle}>
+                    <Ionicons name="shield-checkmark-outline" size={27} color='red' /><Text style={StyleParametre.textPara} onPress={toggleMenu}>Sécurité</Text>
+                </View>
+                {menuVisible && (
+                    <View style={StyleParametre.menu}>
+                        <Text style={StyleParametre.menuText}>PASSWORD</Text>
+                        <Text style={StyleParametre.menuText}>FACE ID</Text>
+                    </View>)}
             </View>
             <View style={StyleParametre.parametreStyle}>
                 <Ionicons name="color-palette-outline" size={27} color='red' /><Text style={StyleParametre.textPara}>Affichage</Text>
             </View>
-            <View style={StyleParametre.parametreStyle}>
-                <Ionicons name="language-outline" size={27} color='red' /><Text style={StyleParametre.textPara}>Langues</Text>
+            <View>
+                <View style={StyleParametre.parametreStyle}>
+                    <Ionicons name="language-outline" size={27} color='red' /><Text style={StyleParametre.textPara} onPress={toggleLangue}>Langues</Text>
+                </View>
+                {langueVisible && (
+                    <View style={StyleParametre.langue}>
+                        <Text style={StyleParametre.menuText}>Choix de langue</Text>
+                        <Text style={StyleParametre.menuText}>Merci de choisir la langue vôtre application</Text>
+                        <View style={StyleParametre.parametreStyle}>
+                            <RadioButton value="anglais" style={{ backgroudColor: 'green' }} /><Text style={{ marginRight: 25 }}>English</Text><Text>Drapeau</Text>
+                        </View>
+                        <View style={StyleParametre.parametreStyle}>
+                            <RadioButton value="français" style={{ backgroudColor: 'green' }} /><Text style={{ marginRight: 25 }}>Français</Text><Text>Drapeau</Text>
+                        </View>
+                        <View style={StyleParametre.parametreStyle}>
+                            <RadioButton value="arabe" style={{ backgroudColor: 'green' }} /> <Text style={{ marginRight: 25 }}>Arabes</Text><Text>Drapeau</Text>
+                        </View>
+                        <View style={StyleParametre.parametreStyle}>
+                            <RadioButton value="espagnol" style={{ backgroudColor: 'green' }} /><Text style={{ marginRight: 25 }}>Espagnol</Text><Text>Drapeau</Text>
+                        </View>
+                    </View>)}
             </View>
             <View style={StyleParametre.parametreStyle}>
                 <Ionicons name="phone-portrait-outline" size={27} color='red' /><Text style={StyleParametre.textPara}>Appareils</Text>
@@ -28,11 +65,26 @@ const StyleParametre = StyleSheet.create({
         display: 'flex',
         flexDirection: 'row',
         alignItems: 'center',
-        paddingBottom: 25,
-        borderBottomColor: 'red',
-        borderBottomWidth: '2'
+        paddingBottom: 25
     },
     textPara: {
         marginLeft: 25
+    },
+    menu: {
+        display: 'flex',
+        alignItems: 'center',
+        fontSize: 22
+    },
+    langue: {
+        display: 'flex',
+        alignItems: 'center',
+        fontSize: 22
+    },
+    menuText: {
+        paddingBottom: 20,
+        borderBottomColor: 'red',
+        borderBottomWidth: '2'
+
+
     }
 })
